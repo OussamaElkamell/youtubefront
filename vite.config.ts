8656f19fd033bf5029497737078ef4023d672d0a
Oussama Elkamell <oussamaaelkamel@gmail.com>
2025-03-19 00:46:32 +0100

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,13 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    proxy: {
+      "/api": {
+        target: "https://youtubebackendd.vercel.app",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   plugins: [
     react(),
